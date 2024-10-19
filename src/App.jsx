2a1f94968acdb3c3aa946befa2ecd5ca85609543
Devansh Raghav <devanshraghavsn@gmail.com>
2024-10-20 00:16:50 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Hero from "./Components/Hero";
+import Projects from "./Components/Projects";
+import "./App.css";
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="App">
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
